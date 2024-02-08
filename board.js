a5e7a3a0b8b585ac2a3d12f4a6657d8a3e28268f
OlegDok <142533766+OlegDok@users.noreply.github.com>
2024-02-08 17:25:19 +0100

    bord made tasks

---
+++ board.js
@@ -0,0 +1,10 @@
+let tasks=[];
+
+
+function renderHtml(){
+    let toDo = document.getElementById("toDo");
+    let inProgress=document.getElementById("inProgress");
+    let awaitFeedback=document.getElementById("awaitFeedback");
+    let done=document.getElementById("done");
+    
+}
