af8d94baa1e257373f6b8237256cb3c72d5cf8c4
ClaudiaDaneder <claudia.daneder@hotmail.com>
2024-02-09 18:17:49 +0100

    test data inserted into JSON array

--- js/addtask.js
+++ js/addtask.js
@@ -1,4 +1,29 @@
-let allTasks = [];
+let allTasks = [
+    {
+        "title": "Beispiel 1",
+        "description": "Beschreibung Beispiel 1",
+        "assignee": "Name 1",
+        "due-date": "2024-03-02",
+        "category": "User Story",
+        "subtasks": "Subtask Beispiel 1"
+    },
+      {
+        "title": "Beispiel 2",
+        "description": "Beschreibung Beispiel 2",
+        "assignee": "Name 2",
+        "due-date": "2024-05-02",
+        "category": "User Story",
+        "subtasks": "Subtask Beispiel 2"
+    },
+      {
+        "title": "Beispiel 3",
+        "description": "Beschreibung Beispiel 3",
+        "assignee": "Name 3",
+        "due-date": "2024-07-18",
+        "category": "Technical Task",
+        "subtasks": "Subtask Beispiel 3"
+    }
+];
 
 let title = document.getElementById('title');
 let description = document.getElementById('description');
@@ -14,7 +39,8 @@ function addNewTask() {
         'description': description.value,
         'assignee': assignee.value,
         'due-date': dueDate.value,
-        'category': category.value
+        'category': category.value,
+        'subtasks': subtasks.value
     }
 
     allTasks.push(task);
@@ -29,6 +55,6 @@ function stringifyJSON() {
 
 function addToSubtaskList() {
     let subtask = subtasks.value;
-    
+
     document.getElementById('subtask-list').innerHTML += '<li>' + subtask + '</li>';
 }
