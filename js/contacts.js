df4f07cd7b824d4c03138f592eb70495864f9ccf
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-02-11 17:57:48 +0100

    storage.js create

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,3 @@
-const STORAGE_TOKEN = 'TJJS7PQGUMX72JU6UWW00YOS9BIWN99TFB2N438Q';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 let contacts = [];
 let firstLetter = [];
 
@@ -244,20 +242,4 @@ function loadLetter() {
 /**Save Storage */
 function saveStorage() {
     localStorage.setItem('allContacts', JSON.stringify(contacts));
-}
-
-async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-        .then(res => res.json());
-}
-
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
-        if (res.data) { 
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
-    });
 }
