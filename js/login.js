a05d355e0bad62ce46d7249701c592ef25f258d2
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-02-13 21:41:30 +0100

    login style

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+let remeChecked = [0];
+
 function logoSize(){
     let login = document.getElementById('login');
     let logoIMG = document.getElementById('logoIMG');
@@ -5,4 +7,34 @@ function logoSize(){
         logoIMG.classList.add('selected');
         login.style.display = 'flex';
     }, 1000);
+}
+
+function rememberMe(){
+    let img = document.getElementById('reme');
+    let host = window.location.protocol + "//" + window.location.host;
+    if(img.src == `${host}/img/unchecked.svg`){
+        img.src = './img/checked.svg';
+        remeChecked = [1];
+    }else{
+        img.src = './img/unchecked.svg';
+        remeChecked = [0];
+    }
+}
+
+function changePWImg(){
+    let img = document.getElementById("changePW");
+    img.src = './img/password_dont.svg';
+    img.setAttribute('onclick','changeShow()');
+}
+
+function changeShow(){
+    let pwInput = document.getElementById("password");
+    let img = document.getElementById("changePW");
+    if(pwInput.type == 'password'){
+        pwInput.setAttribute('type', 'text');
+        img.src = './img/password_show.svg';
+    }else{
+        pwInput.setAttribute('type', 'password');
+        img.src = './img/password_dont.svg';
+    }
 }
