b3a13f7afe25e80e1f9994e9076feb7eaa228cff
ClaudiaDaneder <claudia.daneder@hotmail.com>
2024-03-03 17:00:18 +0100

    jsdoc comments added

--- js/addtask-subtask.js
+++ js/addtask-subtask.js
@@ -1,26 +1,40 @@
-function updateSubtaskButtons(id) {
+/**
+ * This function is used to display different icons on the subtask input based on the value. as sson as an input is made, the buttons to either clear or add a subtask are displayed. 
+ */
+function updateSubtaskButtons() {
     let subtaskField = document.getElementById('subtasks');
     if (subtaskField.value === '') {
         document.getElementById('subtaskfield-buttons').innerHTML = showPlusButton();
 
     } else {
-        document.getElementById('subtaskfield-buttons').innerHTML = showClearOrAddButtons(id);
+        document.getElementById('subtaskfield-buttons').innerHTML = showClearOrAddButtons();
     }
 }
 
+
+/**
+ * This function returns the html code to display the + symbol.
+ * 
+ * @returns string
+ */
 function showPlusButton() {
     return `<button type="button" class="subtaskfield-button-general"><img src="/img/addtask_icon_subtaskfield_plus.svg"></button>`;
 }
 
-function showClearOrAddButtons(id) {
-    let saveSubtask = 'addToSubtasks()';
-    if(id){
-        saveSubtask = `setNewSubTask(${id})`;
-    }
-    return `<button type="button" class="subtaskfield-button-general" onclick="clearSubtaskField()"><img src="/img/addtask_icon_subtaskfield_cancel.svg"></button><hr><button type="button" class="subtaskfield-button-general" onclick="${saveSubtask}"><img src="/img/addtask_icon_subtaskfield_check.svg"></button></div>`;
+
+/**
+ * This function returns the html code to display the buttons to either clear or add a subtask.
+ * 
+ * @returns string
+ */
+function showClearOrAddButtons() {
+    return `<button type="button" class="subtaskfield-button-general" onclick="clearSubtaskField()"><img src="/img/addtask_icon_subtaskfield_cancel.svg"></button><hr><button type="button" class="subtaskfield-button-general" onclick="addToSubtasks()"><img src="/img/addtask_icon_subtaskfield_check.svg"></button></div>`;
 }
 
 
+/**
+ * This function is used to save the current subtask into the array "subtasks". 
+ */
 function addToSubtasks() {
     let subtaskField = document.getElementById('subtasks');
     let subtaskContent = subtaskField.value.trim();
@@ -33,6 +47,9 @@ function addToSubtasks() {
 }
 
 
+/**
+ * This function is used to generate a list of each subtask that is currently stored within the array "subtasks".
+ */
 function generateSubtasklist() {
     let subtaskList = document.getElementById('subtasklist');
     subtaskList.innerHTML = '';
@@ -42,6 +59,14 @@ function generateSubtasklist() {
     }
 }
 
+
+/**
+ * This function generates the html code for each individual subtask, including the buttons to either edit or delete the subtask.
+ * 
+ * @param {number} i 
+ * @param {string} subtask 
+ * @returns string
+ */
 function showSubtaskItem(i, subtask) {
     return `<div class="subtasklist-item" id="subtasklist-item_${i}" ondblclick="editSubtasklistItem(${i})" onmouseenter="showEditButtons(${i})" onmouseleave="showEditButtons(${i})">
     <div class="subtasklist-infos"><div class="subtasklist-marker">•</div>${subtask}</div>
@@ -53,6 +78,11 @@ function showSubtaskItem(i, subtask) {
 }
 
 
+/**
+ * This function is used to show or hide the buttons used to either edit or delete an item from the subtask list when hovering over the respective item.
+ * 
+ * @param {number} i 
+ */
 function showEditButtons(i) {
     let editButtons = document.getElementById(`edit-buttons_${i}`);
     if (editButtons) {
@@ -61,6 +91,13 @@ function showEditButtons(i) {
 }
 
 
+/**
+ * This function is used to transform a subtasklist item into an input field in order to edit the subtasklist item. The cursor is automatically moved to the end of the value.
+ * 
+ * @param {number} s 
+ * @param {number} id 
+ * @param {number} j 
+ */
 function editSubtasklistItem(s, id, j) {
     let listItem = document.getElementById(`subtasklist-item_${s}`);
     
@@ -76,6 +113,15 @@ function editSubtasklistItem(s, id, j) {
     }
 }
 
+
+/**
+ * This function returns html code that is needed to display the input field. Depending on where the function is being used (at addtask or on the board), the buttons to delete or update a subtasklist item each have different functionalities.
+ * 
+ * @param {string} subtaskText 
+ * @param {number} s 
+ * @param {number} id 
+ * @returns string
+ */
 function showSubtaskItemEditField(subtaskText, s, id) {
     let updatebutton = `updateSubtasklistItem(${s})`;
     let deletebutton = `deleteSubtasklistItem(${s})`;
@@ -93,22 +139,38 @@ function showSubtaskItemEditField(subtaskText, s, id) {
 }
 
 
+/**
+ * This function adds a blue border to the subtaskfield when it's in focus.
+ */
 function subtaskfieldFocus() {
     document.getElementById('styled-subtaskfield').classList.toggle('subtaskfield-focus')
 }
 
 
+/**
+ * This function removes the blue border when it's not in focus.
+ */
 function subtaskfieldBlur() {
     document.getElementById('styled-subtaskfield').classList.remove('subtaskfield-focus')
 }
 
 
+/**
+ * This function is used to delete a specific item from the array "subtasks".
+ * 
+ * @param {number} s 
+ */
 function deleteSubtasklistItem(s) {
     subtasks.splice(s, 1);
     generateSubtasklist();
 }
 
 
+/**
+ * This function updates the new value from the input field to the respective item within the subtasklist, and then regenerates the list of subtasks after a short timeout.
+ * 
+ * @param {number} s 
+ */
 function updateSubtasklistItem(s) {
     let editField = document.getElementById('editfield');
     
@@ -119,6 +181,9 @@ function updateSubtasklistItem(s) {
 }
 
 
+/**
+ * This function is used to reset the input field and respective buttons to the default values after a subtask has been added to the list.
+ */
 function clearSubtaskField() {
     let subtaskField = document.getElementById('subtasks');
     subtaskField.value = '';
@@ -126,6 +191,9 @@ function clearSubtaskField() {
 }
 
 
+/**
+ * This function clears the subtask list either when the whole form is being cleared or a new task has been created. 
+ */
 function clearSubtaskList() {
     let subtaskList = document.getElementById('subtasklist');
     subtaskList.innerHTML = '';
--- js/addtask.js
+++ js/addtask.js
@@ -13,8 +13,11 @@ async function initAddTask() {
 }
 
 
+/**
+ * This function defines what constitutes a task, and collects all necessary data to store it. 
+ */
 async function addNewTask() {
-    let taskID = await identifyTaskId();
+    let taskID = await determineTaskId();
     let state = document.getElementById('my-form').value;
     if (state === undefined) {
         state = 'toDos';
@@ -38,6 +41,10 @@ async function addNewTask() {
     redirectToBoard();
 }
 
+
+/**
+ * This function loads all previously saved contacts from storage.
+ */
 async function loadContactsFromStorage() {
     let allContactsAsString = await getItem('allContacts');
     allContacts = JSON.parse(allContactsAsString);
@@ -45,6 +52,11 @@ async function loadContactsFromStorage() {
 }
 
 
+/**
+ * This function first loads the previously saved tasks from storage, then pushes the current task into this array, and then saves all of them to the storage again. 
+ * 
+ * @param {Array} task 
+ */
 async function saveToStorage(task) {
     let existingTasksString = await getItem('allTasks');
     let existingTasks = existingTasksString ? JSON.parse(existingTasksString) : [];
@@ -56,7 +68,12 @@ async function saveToStorage(task) {
 }
 
 
-async function identifyTaskId() {
+/**
+ * This function is used to determine the task-id of the current task, by checking the last task-id in storage, and adding 1 to it, so that they have consecutive, unique IDs.
+ * 
+ * @returns 'task-id' of the current task
+ */
+async function determineTaskId() {
     let allSavedTasks = JSON.parse(await getItem('allTasks'));
     lastID = allSavedTasks[allSavedTasks.length - 1];
     if (lastID == null || lastID == '') {
@@ -68,6 +85,11 @@ async function identifyTaskId() {
 }
 
 
+/**
+ * This function is used to load matching contacts into the contacts dropdown. If no contacts are available, a message will be displayed.
+ * 
+ * @param {Array} filteredContacts 
+ */
 function loadContactsIntoDropdown(filteredContacts) {
     let assignee = document.getElementById('assignee');
     assignee.innerHTML = '';
@@ -84,6 +106,10 @@ function loadContactsIntoDropdown(filteredContacts) {
     }
 }
 
+
+/**
+ * This function registers clicks outside of the contacts dropdown area. If the dropdown is open, the function closes it and hides the input field. 
+ */
 document.addEventListener('click', function (event) {
     let dropdown = document.getElementById('contacts-dropdown');
     let hiddenContactsInput = document.getElementById('hidden-contacts-input');
@@ -102,6 +128,11 @@ document.addEventListener('click', function (event) {
 });
 
 
+/**
+ * This function is used to open or close the contacts dropdown on click.
+ * 
+ * @param {Event} event
+ */
 async function toggleContactsDropdown(event) {
     let dropdown = document.getElementById('contacts-dropdown');
     let dropdownContent = document.getElementById('assignee');
@@ -120,11 +151,25 @@ async function toggleContactsDropdown(event) {
 }
 
 
+/**
+ * This function returns the appropriate html-code in case there are no contacts to display in the contacts dropdown.
+ * 
+ * @returns string
+ */
 function noContactsToShow() {
     return `<label class="checkbox-option no-contacts">No contacts to display. Please add a contact first.</label>`;
 }
 
 
+/**
+ * This function renders the necessary information for all available contacts into the contacts dropdown.
+ * 
+ * @param {string} contactName 
+ * @param {string} contactColor 
+ * @param {string} initials 
+ * @param {number} i 
+ * @returns 
+ */
 function renderContacts(contactName, contactColor, initials, i) {
     return `<label class="checkbox-option" id="checkbox-option_${i}" onclick="changeSelectedContactBackground(${i})">
     <img id="checkbox_${i}" src="./img/addtask_contacts_checkbox_empty.svg">
@@ -132,6 +177,9 @@ function renderContacts(contactName, contactColor, initials, i) {
 }
 
 
+/**
+ * This function transforms the header of the contacts dropdown into an input field to filter contacts on input. 
+ */
 function enableContactsSearchField() {
     let hiddenContactsInput = document.getElementById('hidden-contacts-input');
     hiddenContactsInput.classList.remove('hide');
@@ -139,6 +187,9 @@ function enableContactsSearchField() {
 }
 
 
+/**
+ * This function filters contacts on input. Until an input is made, all saved contacts will be displayed
+ */
 async function filterContacts() {
     let hiddenContactsInput = document.getElementById('hidden-contacts-input');
     let search = hiddenContactsInput.value.toLowerCase();
@@ -153,6 +204,9 @@ async function filterContacts() {
 }
 
 
+/**
+ * This function updates the array of selected contacts based on whether their checkbox has been checked or not. The contact name and their associated color are being registered.
+ */
 function updateSelectedContacts() {
     selectedContacts = [];
 
@@ -168,6 +222,11 @@ function updateSelectedContacts() {
 }
 
 
+/**
+ * This function changes the background color and the image of the checkbox if a contact has been selected within the contacts dropdown. 
+ * 
+ * @param {number} i 
+ */
 function changeSelectedContactBackground(i) {
     let checkboxOption = document.getElementById(`checkbox-option_${i}`);
     checkboxOption.classList.toggle('checkbox-option-selected');
@@ -182,6 +241,9 @@ function changeSelectedContactBackground(i) {
 }
 
 
+/**
+ * This function generates a list of selected contacts in the form of colored circles containing the intials of the respective contacts. 
+ */
 function generateSelectedAssigneesList() {
     let selectedAssignees = document.getElementById('selected-assignees-list');
     selectedAssignees.innerHTML = '';
@@ -195,11 +257,17 @@ function generateSelectedAssigneesList() {
 }
 
 
+/**
+ * This function temporarily stores the array of the selected contacts in the local storage.
+ */
 function saveSelectedContacts() {
     localStorage.setItem('selectedContacts', JSON.stringify(selectedContacts));
 }
 
 
+/**
+ * This function loads the selected contacts from local storage and marks those previously selected ones again, in case the contacts dropdown has been closed and opened up again.
+ */
 function loadSelectedContacts() {
     let savedContacts = localStorage.getItem('selectedContacts');
     if (savedContacts) {
@@ -217,6 +285,9 @@ function loadSelectedContacts() {
 }
 
 
+/**
+ * This function sets the priority-buttons to 'medium' by default. It registers clicks on other priority-buttons and sets the priority accordingly.
+ */
 document.addEventListener('DOMContentLoaded', function () {
     selectPriority('medium');
 
@@ -228,6 +299,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+/**
+ * This function adds or removes the appropriate css class to or from the selected priority button to highlight it correctly.
+ * 
+ * @param {string} priority 
+ */
 function selectPriority(priority) {
     selectedPriority = priority;
     document.querySelectorAll('.prio-button-container button').forEach(button => {
@@ -240,13 +316,21 @@ function selectPriority(priority) {
 }
 
 
+/**
+ * This function registers the selected priority button and sets the 'prio' value in task to the selected one. 
+ * 
+ * @param {Array} task 
+ */
 function checkPriority(task) {
     task.prio = selectedPriority;
 }
 
 
+/**
+ * This function registers clicks on the document. Based on the clicked element, it either opens the user dropdown menu, toggles the category dropdown or closes the dropdown (if the click happens outside of the dropdown area). 
+ */
 document.addEventListener('click', function (event) {
-    if (doc('logoutScreen').style.display == 'flex') {
+    if (document.getElementById('logoutScreen').style.display == 'flex') {
         openUser();
     }
     let dropdown = document.getElementById('category-dropdown');
@@ -264,6 +348,9 @@ document.addEventListener('click', function (event) {
 });
 
 
+/**
+ * This function opens and closes the category dropdown.
+ */
 function toggleCategoryDropdown() {
     let dropdown = document.getElementById('category-dropdown');
     let isActive = dropdown.classList.contains('active');
@@ -277,6 +364,11 @@ function toggleCategoryDropdown() {
 }
 
 
+/**
+ * This function is used to select a value from the category dropdown. As soon as a selection is made, the dropdown is being closed and the selection set as the value of a hidden input field to report it back to "task".
+ * 
+ * @param {string} category 
+ */
 function selectCategory(category) {
     let categoryField = document.getElementById('category-dropdown-text');
     let hiddenCategoryDropdown = document.getElementById('hidden-category-dropdown');
@@ -289,6 +381,9 @@ function selectCategory(category) {
 }
 
 
+/**
+ * This function is used to reset all fields back to their default values.
+ */
 function resetForm() {
     document.getElementById('my-form').reset();
     document.querySelector('.create-button').disabled = false;
@@ -301,16 +396,26 @@ function resetForm() {
 }
 
 
+/**
+ * This function is used to reset the default value of the priority buttons back to 'medium'.
+ */
 function resetPrioButtons() {
     selectPriority('medium');
 }
 
+
+/**
+ * This function is used to reset the category dropdown.
+ */
 function resetCategoryField() {
     document.getElementById('category-arrow').classList.remove('hide')
     document.getElementById('category-dropdown-text').innerHTML = 'Select task category';
 }
 
 
+/**
+ * This function is used to reset the checkboxes and background colors within the contacts dropdown.
+ */
 function resetCheckboxOptions() {
     let checkboxOptions = document.querySelectorAll('.checkbox-option');
     checkboxOptions.forEach(function (checkbox) {
@@ -324,16 +429,26 @@ function resetCheckboxOptions() {
 }
 
 
+/**
+ * This function disables the 'create task' button while the task is being saved to the board.
+ */
 function disableCreateButton() {
     document.querySelector('.create-button').disabled = true;
     document.getElementById('create-button').classList.add('blue-create-button');
 }
 
+
+/**
+ * This function clears the temporarily stored selected contacts from the local storage.
+ */
 function clearLocalStorage() {
     localStorage.removeItem('selectedContacts');
 }
 
 
+/**
+ * This function shows the popup to indicate that the task has been saved.
+ */
 function showPopup() {
     let w = parseInt(window.innerWidth)
     if (w < 850) {
@@ -344,6 +459,9 @@ function showPopup() {
 }
 
 
+/**
+ * This function redirects the user to the board, after a short timeout.
+ */
 function redirectToBoard() {
     setTimeout(function () {
         window.location.href = 'board.html';
@@ -351,6 +469,11 @@ function redirectToBoard() {
 }
 
 
+/**
+ * This function sets the cursor at the end of an input value to make it easier to edit the value.
+ * 
+ * @param {Element} input 
+ */
 function moveCursorToEnd(input) {
     input.focus();
     input.setSelectionRange(input.value.length, input.value.length);
