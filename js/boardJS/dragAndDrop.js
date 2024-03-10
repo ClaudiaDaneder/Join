753b8ed47a2254cd8ddaf691089dd3e048b65266
OlegDok <142533766+OlegDok@users.noreply.github.com>
2024-03-10 17:00:03 +0100

    drag and dropp jsdoc

--- js/boardJS/dragAndDrop.js
+++ js/boardJS/dragAndDrop.js
@@ -1,22 +1,34 @@
+/**
+ * Prevents the default behavior to allow dropping elements.
+ * @param {Event} ev - The dragover event.
+ */
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-
+/**
+ * Initiates the dragging process by setting data and applying styles.
+ * @param {Event} ev - The dragstart event.
+ * @param {string} id - The ID of the element being dragged.
+ */
 function drag(ev, id) {
   const element = document.getElementById(id);
   if (element) {
     element.classList.add("rotating");
+    element.style.zIndex = 1000;  // Setzen Sie den z-index hoch genug, um über anderen Elementen zu sein
     element.addEventListener("dragend", () => {
       element.classList.remove("rotating");
+      element.style.zIndex = "";  // Setzen Sie den z-index zurück, wenn die Drag-Operation endet
     });
   }
   ev.dataTransfer.setData("id", id);
   ev.dataTransfer.dropEffect = "move";
-
 }
 
-
+/**
+ * Handles the drop event by moving the task element to a new column.
+ * @param {Event} ev - The drop event.
+ */
 function drop(ev) {
   ev.preventDefault();
   let taskId = ev.dataTransfer.getData("id");
@@ -34,7 +46,11 @@ function drop(ev) {
   upDateAllDate();
 }
 
-
+/**
+ * Determines the target element based on the event's location.
+ * @param {Event} ev - The event that triggered the function.
+ * @returns {Element} The determined target element.
+ */
 function determineTargetElement(ev) {
   let targetElement = ev.target.classList.contains("noTask")
     ? ev.target.nextElementSibling
@@ -45,12 +61,20 @@ function determineTargetElement(ev) {
   return targetElement;
 }
 
-
+/**
+ * Moves a task element to a specified column.
+ * @param {Element} taskElement - The task element to move.
+ * @param {Element} targetElement - The target column element.
+ */
 function moveTaskToColumn(taskElement, targetElement) {
   targetElement.appendChild(taskElement);
 }
 
-
+/**
+ * Moves a task to a specified container.
+ * @param {Object} task - The task object to move.
+ * @param {string} containerId - The ID of the container to move the task to.
+ */
 function moveTaskToContainer(task, containerId) {
   switch (containerId) {
     case "toDo":
@@ -72,6 +96,11 @@ function moveTaskToContainer(task, containerId) {
   }
 }
 
+/**
+ * Updates the task list after a task has been moved.
+ * @param {string} taskId - The ID of the task that was moved.
+ * @param {string} containerId - The ID of the container to which the task was moved.
+ */
 function updateTaskList(taskId, containerId) {
   let taskToMove = findTaskById(taskId);
   if (!taskToMove) return;
@@ -80,14 +109,21 @@ function updateTaskList(taskId, containerId) {
   moveTaskToContainer(taskToMove, containerId);
 }
 
-
+/**
+ * Finds a task by its ID.
+ * @param {string} taskId - The ID of the task to find.
+ * @returns {Object} The found task object.
+ */
 function findTaskById(taskId) {
   return [...toDos, ...inProgress, ...awaitFeedback, ...done].find(
     (task) => task["task-id"] == taskId
   );
 }
 
-
+/**
+ * Removes a task from its current list.
+ * @param {Object} taskToRemove - The task object to remove.
+ */
 function removeTaskFromCurrentList(taskToRemove) {
   if (toDos.includes(taskToRemove)) {
     toDos = toDos.filter((task) => task["task-id"] !== taskToRemove["task-id"]);
